d7aa01eab4e403f7287347cf1cc423326664a9b4
Petr Kočí <petr.koci@samizdat.cz>
2023-04-02 13:42:45 +0200

    basic layout + buttons

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,43 @@
 import { useState } from "preact/hooks";
+import {
+  Container,
+  Stack,
+  Heading,
+  Center,
+  Image,
+  Button,
+  ButtonGroup,
+} from "@chakra-ui/react";
+import { Icon } from "@iconify/react";
 import "./app.css";
 
 export function App() {
   return (
-    <>
-      <p>ahoj</p>
-    </>
+    <Container maxW={"620px"} centerContent>
+      <Stack spacing={4}>
+        <Heading as={"h1"} size={"xl"}>
+          <Center>Kdo je autorem obrázku?</Center>
+        </Heading>
+        <Image src="https://placehold.co/600x400" borderRadius={8}></Image>
+        <Center>
+          <ButtonGroup size={"lg"} colorScheme={"red"} spacing={4}>
+            <Button
+              leftIcon={
+                <Icon icon="icons8:old-time-camera" width={36} height={36} />
+              }
+            >
+              Fotograf
+            </Button>
+            <Button
+              leftIcon={
+                <Icon icon="teenyicons:robot-solid" width={30} height={30} />
+              }
+            >
+              Umělá inteligence
+            </Button>
+          </ButtonGroup>
+        </Center>
+      </Stack>
+    </Container>
   );
 }
